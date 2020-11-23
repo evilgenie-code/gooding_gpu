a649060fba104118f98813a2787a14add9f8f1ea
Vadim Kravchenko <vadim.kravchenko@pimpay.com>
2020-11-23 11:55:52 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -17,7 +17,9 @@ void getUnit(double* r, double R, double* Unit)
 
 void getUnitDouble(double* r, double R, double* Unit)
 {
-    for (int j = 0; j < 3; j++)
+    int j;
+
+    for (j = 0; j < 3; j++)
     {
         Unit[j] = r[j] / R;
     }
@@ -45,6 +47,53 @@ void cross(double* vet1, double* vet2, double* prod)
 // series approximation to T(x) and its derivatives
 // (used for near - parabolic cases)
 void sigmax(double y, double sig = 0.0, double dsigdx = 0.0, double d2sigdx2 = 0.0, double d3sigdx3 = 0.0)
+{
+    double powers[25];
+    int i;
+    // preload the factors[an]
+    // (25 factors is more than enough for 16 - digit accuracy)
+    double an[25] = { 4.000000000000000e-001,     2.142857142857143e-001,     4.629629629629630e-002,
+                      6.628787878787879e-003,     7.211538461538461e-004,     6.365740740740740e-005,
+                      4.741479925303455e-006,     3.059406328320802e-007,     1.742836409255060e-008,
+                      8.892477331109578e-010,     4.110111531986532e-011,     1.736709384841458e-012,
+                      6.759767240041426e-014,     2.439123386614026e-015,     8.203411614538007e-017,
+                      2.583771576869575e-018,     7.652331327976716e-020,     2.138860629743989e-021,
+                      5.659959451165552e-023,     1.422104833817366e-024,     3.401398483272306e-026,
+                      7.762544304774155e-028,     1.693916882090479e-029,     3.541295006766860e-031,
+                      7.105336187804402e-033 };
+
+    for (i = 0; i < 25; i++)
+    {
+        //powers of y
+        powers[i] = pow(y, i + 1);
+        sig += 4 / 3 + powers[i] * an[i];
+    }
+
+    for (i = 0; i < 24; i++)
+    {
+        // dsigma / dx
+        dsigdx += ((i + 2) * powers[i + 1]) * an[i + 1];
+    }
+
+    for (i = 0; i < 23; i++)
+    {
+        // d2sigma / dx2
+        d2sigdx2 = ((i + 3) * (i + 2) * powers[i + 2]) * an[i + 2];
+    }
+
+    for (i = 0; i < 23; i++)
+    {
+        // d3sigma / dx3
+        d3sigdx3 = ((i + 4) * (i + 3) * (i + 2) * powers[i + 3]) * an[i + 3];
+    }
+
+    dsigdx = dsigdx + an[0];
+    d2sigdx2 = d2sigdx2 + 0 + 2 * an[1];
+    d3sigdx3 = d3sigdx3 + 0 + 0 + 3 * 2 * an[2];
+
+}
+
+void sigmaxGpu(double y, double sig = 0.0, double dsigdx = 0.0, double d2sigdx2 = 0.0, double d3sigdx3 = 0.0)
 {
     double powers[25];
     int i;
@@ -143,6 +192,155 @@ void LancasterBlanchard(double x, double q, double m, double* Tprod)
     }
 }
 
+__global__ void LancasterBlanchardGpu(double x, double q, double m, double* Tprod)
+{
+    int blockIndex = blockIdx.x + blockIdx.y * gridDim.x + blockIdx.z * gridDim.y * gridDim.x;
+
+    int ThreadIndex = threadIdx.x + threadIdx.y * blockDim.x + threadIdx.z * blockDim.y * blockDim.x;
+
+    int tid = blockIndex * blockDim.x * blockDim.y * blockDim.z + ThreadIndex;
+
+    double E, sig1 = 0.0, sig2 = 0.0, dsigdx1 = 0.0,
+            dsigdx2 = 0.0, d2sigdx21 = 0.0, d2sigdx22 = 0.0, d3sigdx31 = 0.0, d3sigdx32 = 0.0,
+            y, z, f, g, d;
+    // protection against idiotic input
+    if (x < -1) //impossible; negative eccentricity
+    {
+        x = abs(x) - 2;
+    }
+    if (x == -1) // impossible; offset x slightly
+    {
+        x = x + 0.00001;
+    }
+
+    // compute parameter E
+    E = x * x - 1;
+
+    if (tid == 0)
+    {
+        if (x == 1) // exactly parabolic; solutions known exactly
+        {
+            Tprod[0] = 4 / 3 * (1 - pow(q, 3));
+        }
+        else if (fabs(x - 1) < 1e-2) // near - parabolic; compute with series
+            // evaluate sigma
+        {
+            sigmaxGpu(-E, sig1, dsigdx1, d2sigdx21, d3sigdx31);
+            sigmaxGpu(-E * q * q, sig2, dsigdx2, d2sigdx22, d3sigdx32);
+            Tprod[0] = sig1 - pow(q, 3) * sig2;
+        }
+        else // all other cases
+        {
+            // compute all substitution functions
+            y = sqrt(fabs(E));
+            z = sqrt(1 + pow(q, 2) * E);
+            f = y * (z - q * x);
+            g = x * z - q * E;
+            d = (E < 0) * (atan2(f, g) + M_PI * m) + (E > 0) * logf(MAX(0, f + g));
+            Tprod[0] = 2 * (x - q * z - d / y) / E;
+        }
+
+    }
+    else if (tid == 1)
+    {
+        if (x == 1) // exactly parabolic; solutions known exactly
+        {
+            Tprod[1] = 4 / 5 * (pow(q, 5) - 1);
+        }
+        else if (fabs(x - 1) < 1e-2) // near - parabolic; compute with series
+            // evaluate sigma
+        {
+            sigmaxGpu(-E, sig1, dsigdx1, d2sigdx21, d3sigdx31);
+            sigmaxGpu(-E * q * q, sig2, dsigdx2, d2sigdx22, d3sigdx32);
+            Tprod[1] = 2 * x * (pow(q, 5) * dsigdx2 - dsigdx1);
+        }
+        else // all other cases
+        {
+            // compute all substitution functions
+            y = sqrt(fabs(E));
+            z = sqrt(1 + pow(q, 2) * E);
+            f = y * (z - q * x);
+            g = x * z - q * E;
+            d = (E < 0) * (atan2(f, g) + M_PI * m) + (E > 0) * logf(MAX(0, f + g));
+            Tprod[1] = (4 - 4 * pow(q, 3) * x / z - 3 * x * (2 * (x - q * z - d / y) / E)) / E;
+        }
+    }
+    else if(tid == 2)
+    {
+        if (x == 1) // exactly parabolic; solutions known exactly
+        {
+            Tprod[2] = (4 / 5 * (pow(q, 5) - 1)) + 120 / 70 * (1 - pow(q, 7));
+        }
+        else if (fabs(x - 1) < 1e-2) // near - parabolic; compute with series
+            // evaluate sigma
+        {
+            sigmaxGpu(-E, sig1, dsigdx1, d2sigdx21, d3sigdx31);
+            sigmaxGpu(-E * q * q, sig2, dsigdx2, d2sigdx22, d3sigdx32);;
+            Tprod[2] = (2 * x * (pow(q, 5) * dsigdx2 - dsigdx1)) / x + 4 * pow(x, 2) * (d2sigdx21 - pow(q, 7) * d2sigdx22);
+        }
+        else // all other cases
+        {
+            // compute all substitution functions
+            y = sqrt(fabs(E));
+            z = sqrt(1 + pow(q, 2) * E);
+            f = y * (z - q * x);
+            g = x * z - q * E;
+            d = (E < 0) * (atan2(f, g) + M_PI * m) + (E > 0) * logf(MAX(0, f + g));
+            Tprod[2] =
+                    (-4 * pow(q, 3) / z * (1 - pow(q, 2) * pow(x, 2) / pow(z, 2)) - 3 * (2 * (x - q * z - d / y) / E) -
+                     3 * x * ((4 - 4 * pow(q, 3) * x / z - 3 * x * (2 * (x - q * z - d / y) / E)) / E)) / E;
+        }
+    }
+    else if(tid == 3)
+    {
+        if (x == 1) // exactly parabolic; solutions known exactly
+        {
+            Tprod[0] = 4.0 / 3 * (1 - pow(q, 3));
+            double Tprod1 = 4.0 / 5 * (pow(q, 5) - 1);
+            double Tprod2 = Tprod1 + 120.0 / 70 * (1 - pow(q, 7));
+            Tprod[3] = 3 * (Tprod2 - Tprod1) + 2400.0 / 1080 * (pow(q, 9) - 1);
+        }
+        else if (fabs(x - 1) < 1e-2) // near - parabolic; compute with series
+            // evaluate sigma
+        {
+            sigmaxGpu(-E, sig1, dsigdx1, d2sigdx21, d3sigdx31);
+            sigmaxGpu(-E * q * q, sig2, dsigdx2, d2sigdx22, d3sigdx32);
+
+            double Tprod1 = 2 * x * (pow(q, 5) * dsigdx2 - dsigdx1);
+            double Tprod2 = Tprod1 / x + 4 * pow(x, 2) * (d2sigdx21 - pow(q, 7) * d2sigdx22);
+            Tprod[3] = 3 * (Tprod2 - Tprod1 / x) / x + 8 * x * x * (pow(q, 9) * d3sigdx32 - d3sigdx31);
+        }
+        else // all other cases
+        {
+            // compute all substitution functions
+            y = sqrt(fabs(E));
+            z = sqrt(1 + pow(q, 2) * E);
+            f = y * (z - q * x);
+            g = x * z - q * E;
+            d = (E < 0) * (atan2(f, g) + M_PI * m) + (E > 0) * logf(MAX(0, f + g));
+            double Tprod0 = 2 * (x - q * z - d / y) / E;
+            double Tprod1 = (4 - 4 * pow(q, 3) * x / z - 3 * x * Tprod0) / E;
+            double Tprod2 = (-4 * pow(q, 3) / z * (1 - pow(q, 2) * pow(x, 2) / pow(z, 2)) - 3 * Tprod0 - 3 * x * Tprod1) / E;
+            Tprod[3] = (4 * pow(q, 3) / pow(z, 2) * ((1 - pow(q, 2) * pow(x, 2) / pow(z, 2)) + 2 * pow(q, 2) * x / pow(z, 2) * (z - x)) - 8 * Tprod1 - 7 * x * Tprod2) / E;
+
+        }
+    }
+}
+
+//double getXM(double xMpi, double phr)
+//{
+//    if (phr < M_PI)
+//    {
+//        return xMpi * pow((phr / M_PI), 0.125);
+//    }
+//    else if (phr > M_PI)
+//    {
+//        return xMpi * (2.0 - pow((2 - phr / M_PI), 0.125));
+//    }
+//
+//    return 0;
+//}
+
 double getXM(double phr, int revs)
 {
     double xMpi = 4 / (3 * M_PI *(2 * revs + 1));
@@ -160,7 +358,7 @@ double getXM(double phr, int revs)
 
 }
 
-double halleysMethod(double xM, double q, int revs)
+__global__ void getXMHalleysMethodGpu(float xM, float q, float revs)
 {
     int iterations;
     const double tol = 1e-11;
@@ -170,7 +368,7 @@ double halleysMethod(double xM, double q, int revs)
     {
         iterations = iterations + 1;
 
-        LancasterBlanchard(xM, q, revs, tProd);
+        LancasterBlanchardGpu<<<1,4>>>(xM, q, revs, tProd);
 
         Tp = tProd[1];
         Tpp = tProd[2];
@@ -189,8 +387,12 @@ double halleysMethod(double xM, double q, int revs)
 //            return ;
 //        }
     }
+}
 
-    return xM;
+
+double halleysMethod(double xM, double q, int revs)
+{
+    getXMHalleysMethodGpu <<<1,1>>>(xM, q, revs);
 }
 
 void lamdert(const float* r1, const float* r2, float t, int revs, int lw, double mu)
@@ -199,7 +401,7 @@ void lamdert(const float* r1, const float* r2, float t, int revs, int lw, double
     double R1 = 0.0, R2 = 0.0, dotProd = 0.0, mcrsProd, theta, c, s, T, q,
             T0 = 0.0, dummy = 0.0;
     double  r1Unit[3], r2Unit[3], crsProd[3], ucrsProd[3], th1Unit[3], th2Unit[3],
-            r1Double[3], r2Double[3], Td, phr, v1[3], v2[3], Tprod[4];
+            r1Double[3], r2Double[3], Tprod0[4] = { T0, 0.0, 0.0, 0.0 }, Td, phr, v1[3], v2[3], Tprod[4];
 
     const double tol = 1e-11;
 
@@ -224,11 +426,8 @@ void lamdert(const float* r1, const float* r2, float t, int revs, int lw, double
     R1 = sqrt(R1);
     R2 = sqrt(R2);
 
-    for (j = 0; j < 3; j++)
-    {
-        r1Unit[j] = r1Double[j] / R1;
-        r2Unit[j] = r1Double[j] / R2;
-    }
+    getUnit(r1Double, R1, r1Unit);
+    getUnit(r2Double, R2, r2Unit);
 
     cross(r1Double, r2Double, crsProd); // перекрестное произведение
 
@@ -256,22 +455,18 @@ void lamdert(const float* r1, const float* r2, float t, int revs, int lw, double
     T = sqrt(8.0 * mu / pow(s, 3)) * t;
     q = (sqrt(R1 * R2) / s) * cos(theta / 2);
 
-    double Tprod0[4] = {T0, 0.0, 0.0, 0.0 };
-
     LancasterBlanchard(0, q, revs, Tprod0);
 
     T0 = Tprod0[0];
-
+    Td = T0 - T;
     phr = fmod(2.0 * atan2(1.0 - pow(q, 2), 2.0 * q), 2.0 * M_PI);
 
-    Td = T0 - T;
-
     // initial output is pessimistic
-//    for (int i = 0;i < 3;i++)
-//    {
-//        v1[i] = 0.0;
-//        v2[i] = 0.0;
-//    }
+    for (int i = 0;i < 3;i++)
+    {
+        v1[i] = 0.0;
+        v2[i] = 0.0;
+    }
 
     double x01;
     double x0, x02, x03, W, lambda, xMpi, xM, Tp;
@@ -393,13 +588,14 @@ void lamdert(const float* r1, const float* r2, float t, int revs, int lw, double
 
     //find root of Lancaster & Blancard's function
     //(Halley's method)
-    printf("x = %f \n", x0);
+    printf("%f \n", x0);
     double x = x0;
     double Tx = 99999;
     int iterations = 0;
 
     while (std::abs(Tx) > tol)
     {
+        iterations = iterations + 1;
         // compute function value, and first two derivatives
 
         LancasterBlanchard(x, q, revs, Tprod);
@@ -413,7 +609,7 @@ void lamdert(const float* r1, const float* r2, float t, int revs, int lw, double
 
         // new value of x
         double xp = x;
-        x = x - (2 * Tx * Tp) / (2 * pow(Tp, 2) - Tx * Tpp);
+        x = x - 2 * Tx * Tp / (2 * pow(Tp, 2) - Tx * Tpp);
         if (iterations % 7 == 0)
         {
             x = (xp + x) / 2;
@@ -423,9 +619,6 @@ void lamdert(const float* r1, const float* r2, float t, int revs, int lw, double
         {
             return;
         }
-
-        iterations++;
-//        printf("x = %f \n ", x);
     }
 
 
@@ -476,15 +669,8 @@ void lamdert(const float* r1, const float* r2, float t, int revs, int lw, double
 
 int main()
 {
-    double AU = 1.49597870691e8;
-    double fMSun = 1.32712440018e11;             // km^3/sec^2
-
-    double UnitR = AU;
-    double UnitV = sqrt(fMSun / UnitR);          // km/sec
-    double UnitT = (UnitR / UnitV) / 86400;         // day
-
     float mu = 1.0;
-    float t = 100.0 / UnitT;
+    float t = 100.0;
     int revs = 0;
     int lw = 1;
     float r1[3] = { -7.8941608095246896e-01, -6.2501194900473045e-01, 3.5441335698377735e-05 };
